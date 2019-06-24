487f4acdd1bedc2edf7d36136b5b71d044f2c36e
brunopescarolli <bruno.pescarolli@outlook.com>
2019-06-24 00:14:40 -0300

    dia da semana

--- menu/js.js
+++ menu/js.js
@@ -1,28 +1,36 @@
-function enviar() {
-    var t1 = window.document.getElementById('txt1')
-    var t2 = window.document.getElementById('txt2')
-    var t3 = window.document.getElementById('txt3')
-    var t4 = window.document.getElementById('txt4')
-    var t5 = window.document.getElementById('txt5')
-    var t6 = window.document.getElementById('txt6')
-    var n1 = String(t1.value)
-    var n2 = Number(t2.value)
-    var n3 = String(t3.value)
-    var n4 = Number(t4.value)
-    var n5 = String(t5.value)
-    var n6 = String(t6.value)
-    var n7 = String(        )
-    var pedido = window.document.getElementById('pedido')
-    if(n1 != n7){
-        if(n2 > 0){
-            pedido.innerHTML = `Seu pedido está sendo preparado e será enviado para: <br> ${n1}, ${n2} ${n3} ${n5}.`
-        }
-    else{
-        pedido.innerHTML = `Endereço inválido.`
-    }
-    }
-    else{
-        pedido.innerHTML = `Endereço inválido.`
+function load(){
+    var data = new Date()
+    var dia = data.getDay()
+    var img = window.document.getElementById('imgjs')
+
+    switch(dia){
+        case 0:
+            txtp.innerHTML = 'CALABRESA'
+            img.src = 'calabresa.png'
+            break
+        case 1:
+            txtp.innerHTML = 'PORTUGUESA'
+            img.src = 'portuguesa.png'
+            break
+        case 2:
+            txtp.innerHTML = 'MARGHERITA'
+            img.src = 'margherita.png'
+            break
+        case 3:
+            txtp.innerHTML = 'QUATRO QUEIJOS'
+            img.src = '4queijos.png'
+            break
+        case 4:
+            txtp.innerHTML = 'FRANGO COM CATUPIRY'
+            img.src = 'frangocatuppiri.png'
+            break
+        case 5:
+            txtp.innerHTML = 'PEPPERONI'
+            img.src = 'pepperoni.png'
+            break
+        case 6:
+            txtp.innerHTML = 'MUSSARELA'
+            img.src = 'mussarela.png'
+            break
     }
 }
-
